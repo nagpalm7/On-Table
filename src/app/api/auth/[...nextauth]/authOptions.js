50ffc5c2e7c6f9a907813391192f83dc98f195c8
Mohit Nagpal <mohitnagpal@Mohits-MacBook-Air.local>
2025-06-21 20:10:21 +0530

    Add database connection while oauth

--- src/app/api/auth/[...nextauth]/authOptions.js
+++ src/app/api/auth/[...nextauth]/authOptions.js
@@ -1,51 +1,52 @@
+import { getDatabaseConnection } from '@/lib/db';
 import Order from '@/model/order';
 import Session from '@/model/session';
 import GoogleProvider from 'next-auth/providers/google';
 import { cookies } from 'next/headers';
 
 export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    })
-  ],
-  callbacks: {
-    async signIn({ user }) {
-      const email = user.email;
-      const cookieStore = await cookies();
-      const sessionId = cookieStore.get('sessionId')?.value;
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+    ],
+    callbacks: {
+        async signIn({ user }) {
+            await getDatabaseConnection();
+            const email = user.email;
+            const cookieStore = await cookies();
+            const sessionId = cookieStore.get('sessionId')?.value;
 
-      if (!sessionId || !email) return true;
+            if (!sessionId || !email) return true;
+            // 1. Upgrade session
+            await Session.findOneAndUpdate(
+                { sessionId },
+                { email },
+                { new: true, upsert: true }
+            );
 
-      // 1. Upgrade session
-      await Session.findOneAndUpdate(
-        { sessionId },
-        { email },
-        { new: true, upsert: true }
-      );
+            // 2. Migrate any orders by sessionId or matching email
+            await Order.updateMany(
+                {
+                    $or: [
+                        { sessionId },
+                        { email: { $exists: true, $eq: email } },
+                    ],
+                },
+                {
+                    email,
+                    sessionId,
+                }
+            );
 
-      // 2. Migrate any orders by sessionId or matching email
-      await Order.updateMany(
-        {
-          $or: [
-            { sessionId },
-            { email: { $exists: true, $eq: email } },
-          ],
+            return true;
         },
-        {
-          email,
-          sessionId,
+
+        async session({ session }) {
+            // You can attach your session info if needed
+            console.log(session);
+            return session;
         }
-      );
-
-      return true;
     },
-
-    async session({ session }) {
-      // You can attach your session info if needed
-      console.log(session);
-      return session;
-    }
-  },
 }
